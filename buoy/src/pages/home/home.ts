c6aff3cbfaeaec1584da685b4b8b00110c0c7ebf
Garry Byrne <1garry8@gmail.com>
2018-04-01 18:32:14 +0100

    removed file

--- buoy/src/pages/home/home.ts
+++ buoy/src/pages/home/home.ts
@@ -1,23 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
 import { AboutPage } from '../about/about';
 import { AngularFireAuth } from 'angularfire2/auth';
-
+import { Geolocation } from '@ionic-native/geolocation';
 import { User } from '../../model/user.model';
 import { LoginPage } from '../login/login';
 
 
+declare var google;
 
 @Component({
-  selector: 'page-home',
+  selector: 'home-page',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
   nowUser : string;
 
-  constructor(
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor( public geolocation: Geolocation,
     public navCtrl: NavController,
     private afAuth : AngularFireAuth
   ) {}
@@ -25,6 +29,7 @@ export class HomePage {
   ionViewDidLoad(){
    this.afAuth.authState.subscribe((res)=>{
      console.log(res);
+     this.loadMap();
    })
   }
   register(){
@@ -33,6 +38,21 @@ export class HomePage {
 
   }
 
+
+ 
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+  }
 
 
 }
